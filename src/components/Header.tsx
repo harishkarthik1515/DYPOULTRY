023c79654dd188fb5402035c6edb3c78650ccc8f
harish karthik <116190257+harishkarthik1515@users.noreply.github.com>
2025-07-09 11:14:58 +0530

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -60,7 +60,7 @@ const Header = () => {
   const rightNavigation = [
     { name: 'Meet the Farm', href: '#meet-the-farm' },
     { name: 'Testimonials', href: '#testimonials' },
-    { name: 'Contact', href: '#contact' },
+    
   ];
 
   const allNavigation = [...leftNavigation, ...rightNavigation];
